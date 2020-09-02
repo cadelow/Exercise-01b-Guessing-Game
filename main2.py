2d5539610ea2a5431e86a3de6f359513af786dd8
cadelow <70243463+cadelow@users.noreply.github.com>
2020-09-01 22:38:45 -0400

    Guessing game and extra credit exercise completed

--- main2.py
+++ main2.py
@@ -1,2 +1,19 @@
 import sys, random
 assert sys.version_info >= (3,8), "This script requires at least Python 3.8"
+attempts = 10
+selected = random.randint(1,100)
+bingo = False
+
+for i in range(attempts):
+    if attempts > 0 and bingo == False:
+        print("You have " + str(attempts) + " attempts left.")
+        guess = input("Guess a number between 0 and 100: ")
+        if int(guess) == selected:
+            bingo = True
+            print(" Congrats! You picked the right number!")
+        else:
+            attempts -= 1
+            if attempts != 0:
+                print("Wrong. Try again!")
+            else:
+                print("You lose. The number was " + str(selected) +".")
